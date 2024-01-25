837a9bf2a3b153027185223ade1abcb03896efa5
Harsh Tyagi <writetoharshtyagi@gmail.com>
2024-01-25 23:50:44 +0530

    feat: add working websocket client to post data to backend node server

---
+++ test.py
@@ -0,0 +1,46 @@
+# Running on Raspberry Pi 4B
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+import asyncio
+import websockets
+import json
+import socketio
+import schedule
+import time
+import random
+
+sio = socketio.Client()
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# SocketIO Client
+
+def job():
+    print("I'm connected to server!")
+    sio.emit('test', {'data': random.random()})  
+
+@sio.event
+def connect():
+    schedule.every(1).seconds.do(job)
+
+    while True:
+        schedule.run_pending()
+
+@sio.on('testResponse')
+def on_test(data):
+    print('I received a response from server!')
+
+    # If you want to access the data sent with the event:
+    print('Reponse : ', data)
+
+# Backend Server
+sio.connect('http://localhost:4000')
+
+if __name__ == '__main__':
+    socketio.run(app)
