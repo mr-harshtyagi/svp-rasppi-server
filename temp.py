7c994a3061f940d0bc64d16847fd952b1e52e2a9
Harsh Tyagi <writetoharshtyagi@gmail.com>
2024-04-05 14:06:07 +0530

    sync

--- temp.py
+++ temp.py
@@ -4,4 +4,4 @@ import Adafruit_DHT
 
 while True:
     humidity, temperature = Adafruit_DHT.read_retry(11, 4)
-    print ('Temp: {0} C  Humidity: {1} %'.format(temperature, humidity))
+    print(temperature,humidity)
