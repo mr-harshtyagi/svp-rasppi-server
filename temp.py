8f624a2a361ba23d81fac452f8d392829aa1fecf
Harsh Tyagi <writetoharshtyagi@gmail.com>
2024-04-05 13:54:06 +0530

    sync

--- temp.py
+++ temp.py
@@ -7,7 +7,7 @@ GPIO.setmode(GPIO.BCM)
 GPIO.cleanup()
 
 # read data using pin 21
-instance = dht11.DHT11(pin = 21)
+instance = dht11.DHT11(pin = 4)
 result = instance.read()
 
 if result.is_valid():
