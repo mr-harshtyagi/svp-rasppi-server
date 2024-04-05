befa24d25fb20b738e22cdfbd6492090942ce53f
Harsh Tyagi <writetoharshtyagi@gmail.com>
2024-04-05 14:35:15 +0530

    sync

--- temp.py
+++ temp.py
@@ -1,13 +1,32 @@
-import Adafruit_DHT
+import os
+import glob
 import time
  
-DHT_SENSOR = Adafruit_DHT.DHT11
-DHT_PIN = 4
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
  
+base_dir = '/sys/bus/w1/devices/'
+device_folder = glob.glob(base_dir + '28*')[0]
+device_file = device_folder + '/w1_slave'
+ 
+def read_temp_raw():
+    f = open(device_file, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+ 
+def read_temp():
+    lines = read_temp_raw()
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lines = read_temp_raw()
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos+2:]
+        temp_c = float(temp_string) / 1000.0
+        temp_f = temp_c * 9.0 / 5.0 + 32.0
+        return temp_c, temp_f
+	
 while True:
-    humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
-    if humidity is not None and temperature is not None:
-        print("Temp={0:0.1f}C Humidity={1:0.1f}%".format(temperature, humidity))
-    else:
-        print("Sensor failure. Check wiring.");
-    time.sleep(2);
+	print(read_temp())	
+	time.sleep(1)
