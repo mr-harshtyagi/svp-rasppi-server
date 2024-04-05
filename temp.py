fd5b910a5d704ffeb330c4c8b6bab4aeee64365e
Harsh Tyagi <writetoharshtyagi@gmail.com>
2024-04-05 14:19:06 +0530

    sync

--- temp.py
+++ temp.py
@@ -1,7 +1,13 @@
-#!/usr/bin/python
-import sys
 import Adafruit_DHT
-
+import time
+ 
+DHT_SENSOR = Adafruit_DHT.DHT11
+DHT_PIN = 4
+ 
 while True:
-    humidity, temperature = Adafruit_DHT.read_retry(11, 4)
-    print(temperature,humidity)
+    humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
+    if humidity is not None and temperature is not None:
+        print("Temp={0:0.1f}C Humidity={1:0.1f}%".format(temperature, humidity))
+    else:
+        print("Sensor failure. Check wiring.");
+    time.sleep(2);
