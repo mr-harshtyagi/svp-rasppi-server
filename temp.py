33e8a4217c21bbc2a10fb761c175c78a3102f75a
Harsh Tyagi <writetoharshtyagi@gmail.com>
2024-05-09 11:17:15 +0530

    sync

--- temp.py
+++ temp.py
@@ -1,31 +1,12 @@
-import os
-import glob
-import time
- 
-os.system('modprobe w1-gpio')
-os.system('modprobe w1-therm')
- 
-base_dir = '/sys/bus/w1/devices/'
-device_folder = glob.glob(base_dir + '28*')[0]
-device_file = device_folder + '/w1_slave'
- 
-def read_temp_raw():
-    f = open(device_file, 'r')
-    lines = f.readlines()
-    f.close()
-    return lines
+from w1thermsensor import W1ThermSensor 
  
 def read_temp():
-    lines = read_temp_raw()
-    while lines[0].strip()[-3:] != 'YES':
-        time.sleep(0.2)
-        lines = read_temp_raw()
-    equals_pos = lines[1].find('t=')
-    if equals_pos != -1:
-        temp_string = lines[1][equals_pos+2:]
-        temp_c = float(temp_string) / 1000.0
-        temp_f = temp_c * 9.0 / 5.0 + 32.0
-        return temp_c, temp_f
+    try:
+        sensor = W1ThermSensor()
+        temperature = sensor.get_temperature()
+        print(f"Temperature: {temperature}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 	
 while True:
 	print(read_temp())	
